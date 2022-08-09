f96343373af8958c23053b400eb35bee9fc1a155
SmartAnubhab <smartanubhab999@gmail.com>
2022-08-09 21:11:20 +0530

    re-written the code for practice purposes

--- src/main/java/com/softech/AddServlet.java
+++ src/main/java/com/softech/AddServlet.java
@@ -1,8 +1,18 @@
 package com.softech;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet{
-	
-	
-	
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		float a=Float.parseFloat(req.getParameter("num1"));
+		float b=Float.parseFloat(req.getParameter("num2"));
+		float result = (a+b);
+		
+		PrintWriter out=res.getWriter();
+		out.println("The result is : " + result);
+	}
 }
