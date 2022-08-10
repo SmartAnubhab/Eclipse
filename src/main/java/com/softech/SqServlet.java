0093ded1d91bae34bb8f642a2bb6ba0cac03ba02
SmartAnubhab <smartanubhab999@gmail.com>
2022-08-10 11:31:28 +0530

    updated SqServlet.java

--- src/main/java/com/softech/SqServlet.java
+++ src/main/java/com/softech/SqServlet.java
@@ -7,7 +7,13 @@ import javax.servlet.http.HttpServletResponse;
 
 public class SqServlet extends HttpServlet{
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		float result=(float)req.getAttribute("r");
+		float temp=result;
+		result *= result;
 		PrintWriter out=res.getWriter();
 		out.println("Print message for SqServlet");
+		out.println("value of k is : " + temp);
+		out.println("So the desired result is " + result);
 	}
 }
