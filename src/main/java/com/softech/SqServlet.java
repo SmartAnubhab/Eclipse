ed802c88b1d43f890e9a499fd7b9e687e25cdec6
SmartAnubhab <smartanubhab999@gmail.com>
2022-08-10 10:15:48 +0530

    added Dispatcher

--- src/main/java/com/softech/SqServlet.java
+++ src/main/java/com/softech/SqServlet.java
@@ -1,10 +1,13 @@
 package com.softech;
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class SqServlet extends HttpServlet{
-	public void service(HttpServletRequest req, HttpServletResponse res) {
-		
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		PrintWriter out=res.getWriter();
+		out.println("Print message for SqServlet");
 	}
 }
