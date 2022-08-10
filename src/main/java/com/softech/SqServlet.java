d120ef392ad125e11508eaef51f21d6f57f6eaa5
SmartAnubhab <smartanubhab999@gmail.com>
2022-08-10 10:01:26 +0530

    created a new servlet named sqServlet

---
+++ src/main/java/com/softech/SqServlet.java
@@ -0,0 +1,10 @@
+package com.softech;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet{
+	public void service(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
+}
