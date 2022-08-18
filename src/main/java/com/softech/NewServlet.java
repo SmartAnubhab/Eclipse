6246d73cc19193a964ea71a9d037a5232d361b86
SmartAnubhab <smartanubhab999@gmail.com>
2022-08-18 23:04:10 +0530

    created servlet

---
+++ src/main/java/com/softech/NewServlet.java
@@ -0,0 +1,14 @@
+package com.softech;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class NewServlet extends HttpServlet{
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		PrintWriter out=res.getWriter();
+	}
+}
