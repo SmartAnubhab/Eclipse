b76624652b0f51c5592bc57aa043a2fe36cba127
SmartAnubhab <smartanubhab999@gmail.com>
2022-08-20 22:28:02 +0530

    updated NewServlet

--- src/main/java/com/softech/NewServlet.java
+++ src/main/java/com/softech/NewServlet.java
@@ -1,7 +1,11 @@
 package com.softech;
 
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class NewServlet extends HttpServlet{
-	
+	public void service(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
 }
