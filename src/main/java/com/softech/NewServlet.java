d8f8368dd4aeef45f062cacec44b2e0c3c42a522
SmartAnubhab <smartanubhab999@gmail.com>
2022-08-20 22:41:05 +0530

    added PrintWriter to the service method

--- src/main/java/com/softech/NewServlet.java
+++ src/main/java/com/softech/NewServlet.java
@@ -1,11 +1,16 @@
 package com.softech;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class NewServlet extends HttpServlet{
-	public void service(HttpServletRequest req, HttpServletResponse res) {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
+		PrintWriter out=res.getWriter();
+		out.println();
 	}
 }
