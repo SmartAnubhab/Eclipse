39dce4955cc66eb10b57b9149c979034448cb0e1
SmartAnubhab <smartanubhab999@gmail.com>
2022-08-21 23:56:14 +0530

    added PrintWriter

--- src/main/java/com/softech/AddServlet.java
+++ src/main/java/com/softech/AddServlet.java
@@ -1,11 +1,14 @@
 package com.softech;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet{
-	public void service(HttpServletRequest req, HttpServletResponse res) {
-		
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		PrintWriter out=res.getWriter();
 	}
 }
