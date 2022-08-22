973b4900006dd35c3918f687240f5df53ff0ee30
SmartAnubhab <smartanubhab999@gmail.com>
2022-08-22 14:55:57 +0530

    fixed add servlet, replaced getattribute with getParameter

--- src/main/java/com/softech/AddServlet.java
+++ src/main/java/com/softech/AddServlet.java
@@ -10,13 +10,13 @@ import jakarta.servlet.http.HttpServletResponse;
 public class AddServlet extends HttpServlet{
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		PrintWriter out=res.getWriter();
-		
+
 		out.println("hello, this is AddServlet");
 		
-		float a=Float.parseFloat((String)req.getAttribute("num1"));
-		float b=Float.parseFloat((String)req.getAttribute("num2"));
+		float a=Float.parseFloat(req.getParameter("num1"));
+		float b=Float.parseFloat(req.getParameter("num2"));
 		float result=a+b;
-		
+
 		out.println("\n\nThe result is : " + result);
 	}
 }
