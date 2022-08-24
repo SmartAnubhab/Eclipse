4f1b6361bad14303999b0644c7269b291cef6586
SmartAnubhab <smartanubhab999@gmail.com>
2022-08-24 18:01:43 +0530

    RequestDispatcher added

--- src/main/java/com/softech/NewServlet.java
+++ src/main/java/com/softech/NewServlet.java
@@ -3,15 +3,21 @@ package com.softech;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class NewServlet extends HttpServlet{
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		
 		PrintWriter out=res.getWriter();
 		out.println("Hello.... \nI am a servlet.");
-		
+		float a=Float.parseFloat(req.getParameter("num1"));
+		float b=Float.parseFloat(req.getParameter("num2"));
+		float result = a + b;
+		RequestDispatcher rd=req.getRequestDispatcher("\sq");
+		rd.forward(req, res);
 	}
 }
