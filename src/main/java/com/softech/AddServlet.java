8ce0e82d97c9d7a089da88480bf279ba2ef4e854
SmartAnubhab <smartanubhab999@gmail.com>
2022-08-24 11:23:07 +0530

    created doGet and doPost method

--- src/main/java/com/softech/AddServlet.java
+++ src/main/java/com/softech/AddServlet.java
@@ -8,7 +8,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet{
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		PrintWriter out=res.getWriter();
+
+		out.println("hello, this is AddServlet");
+		
+		float a=Float.parseFloat(req.getParameter("num1"));
+		float b=Float.parseFloat(req.getParameter("num2"));
+		float result=a+b;
+
+		out.println("\n\nThe result is : " + result);
+		out.println("\nFor loop:");
+		for(int i=0; i<15; i++){
+			if(i==14) {
+				out.println(i*2);
+			}
+		}
+	}
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		PrintWriter out=res.getWriter();
 
 		out.println("hello, this is AddServlet");
