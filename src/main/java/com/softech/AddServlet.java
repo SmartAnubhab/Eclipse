ba3c5e5017864bf02bad68e048bec2067c00764d
SmartAnubhab <smartanubhab999@gmail.com>
2022-10-10 17:57:38 +0530

    updated Add Servlet

--- src/main/java/com/softech/AddServlet.java
+++ src/main/java/com/softech/AddServlet.java
@@ -3,24 +3,31 @@ package com.softech;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import org.apache.tomcat.util.http.parser.Cookie;
+
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet{
-	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-
-		HttpSession session=req.getSession();
-		float k=(float)session.getAttribute("result");
-		float a=(float)session.getAttribute("a");
-		float b=(float)session.getAttribute("b");
-		session.removeAttribute("result");
-		
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		PrintWriter out=res.getWriter();
-		out.println("Input 1 : " + a);
-		out.println("Input 2 : " + b);
-		out.println("The value passed here is : " + k);
+		float k=0;
+		out.println("Initial value of k is : " + k);
+		jakarta.servlet.http.Cookie[] cookies=req.getCookies();
+		
+		for	(jakarta.servlet.http.Cookie c: cookies) {
+			if(c.getName().equals("result")) {
+				k=Float.parseFloat(c.getValue());
+				out.println("The value passed here is : " + k);
+			}
+		}
+		
+		
+//		out.println("Input 1 : " + a);
+//		out.println("Input 2 : " + b);
+		
 		
 	}
 }
