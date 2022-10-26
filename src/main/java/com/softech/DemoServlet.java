ef8b19b0d02b6935cea29a1460323cc613a8bca6
SmartAnubhab <smartanubhab999@gmail.com>
2022-10-26 22:52:45 +0530

    Request Dispatcher added

--- src/main/java/com/softech/DemoServlet.java
+++ src/main/java/com/softech/DemoServlet.java
@@ -1,5 +1,13 @@
 package com.softech;
 
-public class DemoServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+public class DemoServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException{
+		
+		
+		RequestDispatcher rd=req.getRequestDispatcher("DemoJsp.jsp")
+	}
 }
